df53c56ec6675e2c7f1ae30b0086713f27bdbaca
ygref <jason.wesley.hc@gmail.com>
2023-10-31 14:08:04 -0400

    fixing iteration for price search and image search

--- src/Content.jsx
+++ src/Content.jsx
@@ -30,22 +30,49 @@ export function Content() {
 
           {searchResults.data.map((result) => (
             <div key={result.id}>
-              <img src={result.image_uris.small} />
-
-              <p>Card Name: {result.name}</p>
-              <p>Mana Cost: {result.mana_cost}</p>
-              <p>Card Type: {result.type_line}</p>
-              <p>Oracle text: {result.oracle_text}</p>
-              <p>
-                Power and Toughness: {result.power}/{result.toughness}
-              </p>
-              <p>Loyalty: {result.loyalty}</p>
-              <p>Sets: {result.set_name}</p>
-              <p>Price USD: {result.prices.usd}</p>
-
-              <p>
-                <a href={result.purchase_uris.tcgplayer}>TCGPlayer: </a>{" "}
-              </p>
+              {result.card_faces ? (
+                <div>
+                  {result.card_faces.map((card_faces, index) => (
+                    <div key={index}>
+                      <img src={card_faces.image_uris.small} />
+                      <p>Card Name: {card_faces.name}</p>
+                      <p>Mana Cost: {card_faces.mana_cost}</p>
+                      <p>Card Type: {card_faces.type_line}</p>
+                      <p>Oracle text: {card_faces.oracle_text}</p>
+                      <p>
+                        Power and Toughness: {card_faces.power}/{card_faces.toughness}
+                      </p>
+                      <p>Loyalty: {card_faces.loyalty}</p>
+                      <p>Sets: {card_faces.set_name}</p>
+                      <p>
+                        Price USD:{" "}
+                        {card_faces.prices && card_faces.prices.usd
+                          ? card_faces.prices.usd
+                          : result.prices && result.prices.usd
+                          ? result.prices.usd
+                          : "N/A"}
+                      </p>
+                    </div>
+                  ))}
+                </div>
+              ) : (
+                <div>
+                  <img src={result.image_uris.small} />
+                  <p>Card Name: {result.name}</p>
+                  <p>Mana Cost: {result.mana_cost}</p>
+                  <p>Card Type: {result.type_line}</p>
+                  <p>Oracle text: {result.oracle_text}</p>
+                  <p>
+                    Power and Toughness: {result.power}/{result.toughness}
+                  </p>
+                  <p>Loyalty: {result.loyalty}</p>
+                  <p>Sets: {result.set_name}</p>
+                  <p>Price USD: {result.prices.usd}</p>
+                  <p>
+                    <a href={result.purchase_uris.tcgplayer}>Purchase on TCGPlayer</a>{" "}
+                  </p>
+                </div>
+              )}
             </div>
           ))}
         </div>
